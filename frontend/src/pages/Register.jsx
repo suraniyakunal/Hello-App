0d42f3caf60cc15ce8d806d3e309de1f8b74082a
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-06-24 21:40:40 +0530

    added some files

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -19,18 +19,18 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/login')
-
+    //    navigate('/login')
+    alert("The form is working fine!")
   }
 
 
   return (
-    <div className=''>
+    <div className='bg-gray-800 items-center justify-center'>
       <form className='flex flex-col justify-center items-center'>
         <input className='p-2 m-2 rounded-md' onChange={handleChange} required type="text" name="name" value={form.username} placeholder='Enter your name..' />
-        <input onChange={handleChange} required type="email" name="email" value={form.email} placeholder='Enter your email..' />
-        <input onChange={handleChange} required type="password" name="password" value={form.password} placeholder='Enter your password..' />
-        <button className='bg-teal-400 text-teal-100 p-2 m-2 rounded-md' type="submit" onClick={handleSubmit}>Signup</button>
+        <input className='p-2 m-2 rounded-md' onChange={handleChange} required type="email" name="email" value={form.email} placeholder='Enter your email..' />
+        <input className='p-2 m-2 rounded-md' onChange={handleChange} required type="password" name="password" value={form.password} placeholder='Enter your password..' />
+        <button className='bg-teal-400 text-teal-100 p-2 m-2 rounded-md hover:bg-teal-950 hover:text-teal-50' type="submit" onClick={handleSubmit}>Signup</button>
 
       </form>
     </div>
