481bb22f23625cf5c1ea1fb9c69dd9773d85e478
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-06-27 03:33:19 +0530

    added some files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
 
 const app = express()
 const port = 3000
 
+//MIDDLEWARE
+app.use(express.json())
+app.use(cors())
+dotenv.config()
 
 app.get('/', (req, res) => {
-  res.json("Hello from the server")
+
+  const user = {
+    name: "kunal",
+    age: 23
+  }
+
+  res.json(user)
 })
+
+
 
 app.listen(port, () => {
   console.log(`The server is live on port ${port}`)
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,36 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 export default function Home() {
 
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/')
+      .then((response) => {
+        setData(response.data)
+      })
+      .catch((error) => {
+        console.log('Error fetching the data', error)
+      })
+
+  }, [])
+
 
   return (
-    <div>
+    <div className='bg-teal-300 items-center justify-center text-black'>
       <h1>This is the Home Page </h1>
+      <div className='flex flex-col '>
+        {data ? (
+          <>
+            <h2>Name: {data.name}</h2>
+            <h2>Age: {data.age}</h2>
+          </>
+        ) : (
+          <p>Loding...</p>
+        )}
+
+      </div>
     </div>
   )
 }
