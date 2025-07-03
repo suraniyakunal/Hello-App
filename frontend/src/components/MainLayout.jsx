6669c382b9b6d5994b6e0c1c8f1c6d8017a4bbf3
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-07-04 03:03:53 +0530

    modified two files

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -6,15 +6,18 @@ import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
   return (
-    <div id="main" className='bg-black w-[100%] relative'>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
+    <div id="main" className='bg-black relative min-h-screen'>
+      <div className="flex flex-col">
+        <div id="nav">
+          <Navbar />
+        </div>
+
         <main className="">
           <Outlet /> {/* This renders the current route’s page */}
         </main>
-        <div id="footer" className='absolute align-bottom justify-center'>
-          <Footer />
-        </div>
+        {/* <div id="footer" className='absolute align-bottom justify-center'> */}
+        {/*   <Footer /> */}
+        {/* </div> */}
       </div>
 
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,20 +4,23 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
-  const navLinkStyles = ({ isActive }) =>
-    isActive
-      ? "text-blue-200 font-semibold border-b-2 border-white"
-      : "hover:text-blue-300";
-
+  // const navLinkStyles = ({ isActive }) =>
+  //   isActive
+  //     ? "text-blue-200 font-semibold border-b-2 border-white"
+  //     : "hover:text-blue-300";
+  //
 
   return (
-    <nav className="bg-emerald-500 text-white px-4 py-3 flex justify-between items-center shadow">
-      <h1 className="text-2xl font-bold">ChatApp</h1>
+    <nav className='w-8 bg-white text-black opacity-5'>
+      <div id="logo">
+        <h1 className="text-2xl font-bold">ChatApp</h1>
 
-      <ul className="hidden md:flex gap-6 text-lg">
-        <li className="hover:text-blue-300 cursor-pointer"><NavLink to='/' className={navLinkStyles}>Home</NavLink></li>
-        <li className="hover:text-blue-300 cursor-pointer"><NavLink to='/chat' className={navLinkStyles}>Chat</NavLink></li>
-        <li className="hover:text-blue-300 cursor-pointer"><NavLink to='/register' className={navLinkStyles}>Register</NavLink></li>
+      </div>
+
+      <ul className="gap-6 text-lg">
+        <li className="hover:text-blue-300 cursor-pointer"><NavLink to='/'>Home</NavLink></li>
+        <li className="hover:text-blue-300 cursor-pointer"><NavLink to='/chat'>Chat</NavLink></li>
+        <li className="hover:text-blue-300 cursor-pointer"><NavLink to='/register'>Register</NavLink></li>
       </ul>
 
       {/* Hamburger for mobile */}
